c56bb75d82d1c7ff401472438626b0b71c20743d
Iury Dias Coelho <148260236+IuryDiasCoelho@users.noreply.github.com>
2025-11-19 21:32:17 -0300

    Update numero_10.py

--- numero_10.py
+++ numero_10.py
@@ -1,13 +1,14 @@
-vel=input('Informe a velocidade do carro (em km/hr):')
+vel=int(input('Informe a velocidade do carro (em km/hr):'))
 vel_max=80
+execo=vel-vel_max
 print(f"você estava a {vel}")
 print(f"A velocidade máxima permitida {vel_max}")
 
-if vel>="90":
-    print("infração leve 3 pontos")
-elif vel>="111":
-    print("infração media 4 pontos")
-elif vel>="120" :
+if execo>=20:
     print("infração grave 7 pontos")
+elif execo>=11:
+    print("infração media 4 pontos")
+elif execo :
+    print("infração leve 3 pontos")
 else:
     print("sem multa 0 pontos")
