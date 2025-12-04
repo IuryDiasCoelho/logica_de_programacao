59676aaf9418c7fef7bb5667c3476e92ee6f9954
IuryDiasCoelho <iurydiascoelho31@hotmail.com>
2025-12-04 17:46:43 -0300

    adequação da questão

--- atividade_05/numero_02.py
+++ atividade_05/numero_02.py
@@ -1,5 +1,11 @@
-from numero_01 import *
+numeros=[]
+repeticao=1
+while repeticao<=5:
+    repeticao+=1
+    entrada=float(input("digite um numero: "))
+    numeros.append(entrada)
 
+print("sua list de numeros é:",numeros)
 print("seu numero maior é:  ",max(numeros))
 print("seu numero menor é:  ",min(numeros))
 print("seu soma dos números é:",sum(numeros))
